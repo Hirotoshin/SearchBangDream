312ad516632ee643198ba1f8df61afe4741cfdd0
Hirotoshin <hirotoshin0211@gmail.com>
2018-08-08 01:24:27 +0900

    [fix]ボタンとテキストの位置変更

--- Form.js
+++ Form.js
@@ -1,30 +1,59 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 export class Form extends Component{
+    constructor(props){
+        super(props);
+
+        this.state ={
+          value:'',
+          message:''
+        };
+    }
+
+    handleInput({target:{value}}){
+        console.log("handle push");
+        this.setState({
+            value
+        });
+}
+    send(){
+        console.log("send push");
+        const {value} = this.state;
+        this.setState({
+            value:'',
+            message:value
+        });
+    }
 
     render(){
         var colorstyle = {
             color :"red",
             backgroundColor:"black",
-            padding:"40px 580px"
+            padding:"40px 780px"
         };
 
         var textsize = {
-            width:"300px",
-            height:"100px"
+            position:"absolute",
+            right:"90px",
+            top:"60px",
+            width:"200px",
+            height:"50px"
         }
 
         var buttonsize = {
-            width:"100px",
-            height:"100px"
+            position:"absolute",
+            right:"40px",
+            top:"60px",
+            width:"50px",
+            height:"50px"
+
         }
         return(
             <div>
                 <h1 style={colorstyle}>Bang Dream</h1>
-                <input type="text" style={textsize}/>
-                <button style={buttonsize}>test</button>
+                <input type="text" value={this.state.value} onChange={this.handleInput.bind(this)} style={textsize}/>
+                <button onClick={this.send.bind(this)} style={buttonsize}>SEND</button>
             </div>
         );
     }
