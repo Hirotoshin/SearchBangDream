f3054fb3f4325b7fc56cff0a137e122dbf29ea1f
Hirotoshin <hirotoshin0211@gmail.com>
2018-08-08 01:39:44 +0900

    [fix]バンド名の追加

--- Form.js
+++ Form.js
@@ -49,11 +49,51 @@ export class Form extends Component{
             height:"50px"
 
         }
+        var popipa ={
+            position:"absolute",
+            left:"40px",
+            top:"160px",
+            width:"100px",
+            height:"50px"
+        }
+        var roselia ={
+            position:"absolute",
+            left:"210px",
+            top:"160px",
+            width:"100px",
+            height:"50px"
+        }
+        var pasupare ={
+            position:"absolute",
+            left:"370px",
+            top:"160px",
+            width:"100px",
+            height:"50px"
+        }
+        var afuro ={
+            position:"absolute",
+            left:"540px",
+            top:"160px",
+            width:"100px",
+            height:"50px"
+        }
+        var harohapi ={
+            position:"absolute",
+            left:"710px",
+            top:"160px",
+            width:"100px",
+            height:"50px"
+        }
         return(
             <div>
                 <h1 style={colorstyle}>Bang Dream</h1>
                 <input type="text" value={this.state.value} onChange={this.handleInput.bind(this)} style={textsize}/>
                 <button onClick={this.send.bind(this)} style={buttonsize}>SEND</button>
+                <button onClick={this.send.bind(this)} style={popipa}>Poppin'Party</button>
+                <button onClick={this.send.bind(this)} style={roselia}>Roselia</button>
+                <button onClick={this.send.bind(this)} style={pasupare}>Pastel Palette</button>
+                <button onClick={this.send.bind(this)} style={afuro}>After glow</button>
+                <button onClick={this.send.bind(this)} style={harohapi}>ハロー、ハッピーワールド！</button>
             </div>
         );
     }
